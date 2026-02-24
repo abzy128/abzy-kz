4b846299a100c69ee1789b753fc507d280e1883d
Abzal Orazbek <abzalorazbek2003@gmail.com>
2026-02-24 15:58:07 +0500

    change email

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "Abzal Orazbek",
-  EMAIL: "abzalorazbek2003@gmail.com",
+  EMAIL: "me@abzy.kz",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 1,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
